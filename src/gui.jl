a123327d6868671176255d4274762fbe4cc89355
Telemaco Terzi <tezze.terzi01@gmail.com>
2024-06-02 12:23:46 +0200

    changed graphics

--- src/gui.jl
+++ src/gui.jl
@@ -17,34 +17,67 @@ app = dash(external_stylesheets=external_stylesheets)
 
 # Define the layout of the app
 app.layout = html_div() do
-    html_h1("Progetto Metodi Calcolo Scientifico"),
-    dcc_upload(
-        id="upload-image",
-        children=html_div([
-            "Drag and Drop or ",
-            html_a("Select Files")
-        ]),
-        style=Dict(
-            "width" => "100%",
-            "height" => "60px",
-            "lineHeight" => "60px",
-            "borderWidth" => "1px",
-            "borderStyle" => "dashed",
-            "borderRadius" => "5px",
-            "textAlign" => "center",
-            "margin" => "10px"
-        ),
-        multiple=false
-    ),
     html_div(
-        id="input-size-block",
+        id="page",
         children=[
-            html_h5("Size of the block\t", style=Dict("display" => "inline-block")),
-            dcc_input(id="size-block", type="number", placeholder="Size of the block", value=1, min=1, max=100, step=1, style=Dict("display" => "inline-block", "margin" => "10px"))
+            html_h1("Progetto Metodi Calcolo Scientifico"),
+            dcc_upload(
+                id="upload-image",
+                children=html_div([
+                    "Drag and Drop or ",
+                    html_a("Select Files")
+                ]),
+                style=Dict(
+                    
+                    "lineHeight" => "60px",
+                    "borderWidth" => "1px",
+                    "borderStyle" => "dashed",
+                    "borderRadius" => "5px",
+                    "textAlign" => "center",
+                    "margin" => "10px"
+                ),
+                multiple=false
+            ),
+            html_div(
+                children=[
+                    html_div(
+                        id="input-size-block",
+                        children=[
+                            html_h5("Size of the block\t", style=Dict("display" => "inline-block")),
+                            dcc_input(id="size-block", type="number", placeholder="Size of the block", value=1, min=1, max=100, step=1, style=Dict("display" => "inline-block", "margin" => "10px"))
+                        ],
+                        style=Dict("display" => "inline-block", "width" => "100vg", "padding" => "10px")
+                    ),
+                    html_div(
+                        id="Compression index",
+                        children=[
+                            html_h5("Index of compression\t", style=Dict("display" => "inline-block")),
+                            dcc_input(id="compression-index", type="number", placeholder="Compression index", value=0, min=1, max=100, step=1, style=Dict("display" => "inline-block", "margin" => "10px"))
+                        ],
+                        style=Dict("display" => "inline-block", "width" => "100vg", "padding" => "10px")
+                    ),
+                ],
+                style=Dict("display" => "flex", "flex-direction" => "row", "justify-content" => "center")
+            ),
+            html_div(
+                children=[
+                    html_div(
+                        id="output-image-upload",
+                        style=Dict("display" => "flex", "flex-direction" => "column", "justify-content" => "center", "width" => "50%")
+                    ),
+                    html_div(
+                        id="processing-image-upload",
+                        style=Dict("display" => "flex", "flex-direction" => "column", "justify-content" => "center", "width" => "50%")
+                    )
+                ],
+                style=Dict("display" => "flex", "flex-direction" => "row", "justify-content" => "center", "max-width" => "100vw")
+            )
         ],
-        style=Dict("display" => "inline-block", "width" => "100vg", "padding" => "10px")),
-    html_div(id="output-image-upload"),
-    html_div(id="processing-image-upload")
+        style=Dict("display" => "flex", "flex-direction" => "column")
+    )
+    
+    
+    
 end
 
 function parse_contents_fig(contents, filename, date)
@@ -53,14 +86,15 @@ function parse_contents_fig(contents, filename, date)
         html_h6(date),
         # HTML images accept base64 encoded strings in the same format
         # that is supplied by the upload
-        html_img(src=contents, style=Dict("height" => "50%", "width" => "50%")),
+        html_img(src=contents, style=Dict("height" => "auto", "width" => "auto")),
         html_hr(),
         html_div("Raw Content"),
         html_pre(string(first(contents, 100), "..."), style=Dict(
             "whiteSpace" => "pre-wrap",
             "wordBreak" => "break-all"
         ))
-    ])
+    ],
+    style=Dict("display" => "flex", "flex-direction" => "column", "justify-content" => "center", "padding" => "20px"))
 end
 
 callback!(
@@ -88,14 +122,10 @@ callback!(
 ) do size_block, contents, filename
     if !(contents isa Nothing)
         println("Processing image")
-        
-        open("code.txt", "w") do f
-            write(f, contents)
-        end
-        contents_img = split(contents, ",")
 
-        img_data = Base64.base64decode(contents_img[2])
-        println("Image data: ", typeof(img_data))
+        img_data = Base64.base64decode(split(contents, ",")[2])
+
+        println("Save image on FS")
         temp_filename = "temp_image.bmp"
         open(temp_filename, "w") do f
             write(f, img_data)
@@ -103,13 +133,14 @@ callback!(
 
         # Load the image
         img = Utils.LoadBmpImage(temp_filename)
-        println("Image loaded: ", typeof(img))
         # Apply the DCT2
+        println("Apply compression")
         out = Dct2.ApplyDct2OnImage(img, size_block, 50)
         # Save the image
+        println("Save image compressed")
         Utils.SaveBmpImage(out, "output.bmp")
-        println("Image saved")
         
+        println("Prepare output")
         output_img = Utils.LoadBmpImage("output.bmp") 
         img_path = "output.bmp"
         img = open(img_path) do file
